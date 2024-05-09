862233b4853e913cbd3628611bee44ca2ac709a9
Serdar Umut TURAN <serdarumuturan@gmail.com>
2024-05-09 15:13:17 +0300

    toplama_cıkarma dosayası silindi!

--- toplama_cıkarma/toplama_cıkarma/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-int toplam, x, y;
-x = 5;
-y = 4;
-toplam = x + y;
-Console.WriteLine(toplam);
